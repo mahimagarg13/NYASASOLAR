663d270e1b165ca940f0a2cdd71ef3c5175dc1f4
Mahima garg <mahimagarg832@gmail.com>
2021-10-08 15:30:15 +0530

    0330

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const app = express();
 const cors = require("cors");
 const pool = require("./db");
 const nodemailer = require("nodemailer");
-
-
-
+const path = require("/path");
+const PORT = process.env.PORT || 5000;
+//process.env.PORT
+//process.env.NODE_ENV => production or undefined
 app.use(cors());
 app.use(express.json());
+
+if(process.env.NODE_ENV === "production") {
+    //server static content
+    //npm run build
+    app.use(express.static(path.join(__dirname, "client/build")));
+}
+
+console.log(__dirname);
 
 //routes//
 //create a email
@@ -189,6 +198,6 @@ app.get("/infos/:id", async (req, res) => {
     }
 });
 
-app.listen(5000, () => {
-    console.log("server has started on port 5000");
+app.listen(PORT, () => {
+    console.log(`server has started on port ${PORT}`);
 });
