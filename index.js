7ba0c82c45060233680e1ce4f8f719153bc5b2dc
Mahima garg <mahimagarg832@gmail.com>
2021-10-11 12:24:27 +0530

    1

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 //process.env.PORT
 
-
 app.use(cors());
 app.use(express.json());
 app.use(express.json("./client/build"));
+
 
 if (process.env.NODE_ENV === "production") {
 
